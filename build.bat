cd51cbecc69c756a8c44c3f082d16892befff5f7
Henrik <henke96@bredband.net>
2020-03-08 18:27:33 +0100

    Clean before building

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 dir /s /B *.java > sources.txt
 
+rmdir /s /q output
 mkdir output
 javac -source 1.8 -target 1.8 @sources.txt -d output
 
--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 find -name "*.java" > sources.txt
 
+rm -rf output
 mkdir output
 javac -source 1.8 -target 1.8 @sources.txt -d output
 
